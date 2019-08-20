a0516f315a20ca991f1a31c7761c5267b36ef456
Pumudi Jayasinghe <43135992+pumudijay@users.noreply.github.com>
2019-08-20 17:27:10 +0530

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Siyago Travels|Travel Agent in Sri Lanka</title>
-  <link rel="icon" type="image ico" href="logo.png" />  
+  <link rel="icon" type="image ico" href="asserts/images/logo.png" />  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
